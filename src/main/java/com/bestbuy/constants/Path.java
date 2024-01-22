92e823eaace641580feffc3e419c2b56f882c962
kaku5700 <primekaku@gmail.com>
2024-01-22 22:15:02 +0000

    project-1-bestbuy-serenity

---
+++ src/main/java/com/bestbuy/constants/Path.java
@@ -0,0 +1,17 @@
+package com.bestbuy.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+
+    /*
+     * This is base path for student api
+     */
+    // This path for stores API
+    public static final String STORES = "/stores";
+
+    // This path for products API
+    public static final String PRODUCTS = "/products";
+
+}
